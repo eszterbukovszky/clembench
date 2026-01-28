e44c3acd72fdb11af7d2739d413698f8aa5f9079
Eszter Bukovszky <eszter.bukovszky@uni-potsdam.de>
2026-01-28 14:16:12 +0100

    taboo lang_config and hu test word list

---
+++ taboo/resources/lang_config.py
@@ -0,0 +1,14 @@
+LANG_CONFIG = {
+    "en": {
+        "CLUE": "CLUE",
+        "GUESS": "GUESS",
+        "CLUE_PREFIX": "CLUE: ",
+        "GUESS_PREFIX": "GUESS: "
+    },
+    "hu": {
+        "CLUE": "NYOM",
+        "GUESS": "TIPP",
+        "CLUE_PREFIX": "NYOM: ",
+        "GUESS_PREFIX": "TIPP: "
+    }
+}
